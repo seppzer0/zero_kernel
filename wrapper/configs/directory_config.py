d9878b30c264c01551b952e5bd65b1ea58b0b8f8
seppzer0 <s3ppzer0@gmail.com>
2024-02-16 22:29:01 +0200

    style: update docstring description

--- wrapper/configs/directory_config.py
+++ wrapper/configs/directory_config.py
@@ -3,7 +3,7 @@ from pydantic.dataclasses import dataclass
 
 @dataclass
 class DirectoryConfig:
-    """A config for local directories used throughout the app."""
+    """A config for key directory paths."""
     root: Path = Path(__file__).absolute().parents[2]
     kernel: Path = Path(root, "kernel")
     assets: Path = Path(root, "assets")
